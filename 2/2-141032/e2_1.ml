0428dd6c226e91e38b89dff2fb04356a5e6348de
yahiro.wataru <wtr816@gmail.com>
2014-04-21 23:57:43 +0900

    impliment add mul over church numeral

--- 2/2-141032/e2_1.ml
+++ 2/2-141032/e2_1.ml
@@ -2,6 +2,12 @@ type church = {t: 'a. ( 'a->'a ) -> 'a -> 'a};;
 
 exception Negative_number;;
 
+let add c1 c2 =
+  {t = fun f x -> c1.t f (c2.t f x)};;
+
+let mul c1 c2 =
+  {t = fun f x -> c1.t (c2.t f) x};;
+
 let rec i2c i = 
     if i < 0 then
       raise Negative_number
