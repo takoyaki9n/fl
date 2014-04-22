24f7216e4502a274c207ee253158fc949af84866
yahiro.wataru <wtr816@gmail.com>
2014-04-22 12:55:04 +0900

    pow and sub over church

--- 2/2-141032/e2_1.ml
+++ 2/2-141032/e2_1.ml
@@ -2,22 +2,34 @@ type church = {t: 'a. ( 'a->'a ) -> 'a -> 'a};;
 
 exception Negative_number;;
 
+let zero = {t = fun f x -> x};;
+
+let scc c =
+  {t = fun f x -> f (c.t f x)};;
+
+let pred c =
+  {t = fun f x -> c.t (fun g h -> h (g f)) (fun u -> x) (fun u -> u)};;
+  
 let add c1 c2 =
   {t = fun f x -> c1.t f (c2.t f x)};;
+
+let sub c1 c2 =
+  {t = fun f x -> ((c2.t pred) c1).t f x};;
 
 let mul c1 c2 =
   {t = fun f x -> c1.t (c2.t f) x};;
 
+let pow c1 c2 =
+  {t = fun f x -> (c2.t c1.t) f x};;
+
 let rec i2c i = 
-    if i < 0 then
-      raise Negative_number
+  if i < 0 then
+    raise Negative_number
+  else
+    if i = 0 then
+      zero
     else
-      if i = 0 then
-	{t=  fun f x -> x}
-      else
-	let c = i2c (i - 1)
-	in
-	{t = fun f x -> f ((c.t f) x)};;
+      scc (i2c (i - 1));;
 
 let c2i c =
   c.t (fun x -> x + 1) 0;;
