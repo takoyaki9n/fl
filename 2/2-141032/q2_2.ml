e996d4136f9c942aed418121bb5fd8607f8ab2cf
yahiro.wataru <wtr816@gmail.com>
2014-04-22 19:55:10 +0900

    report2

--- 2/2-141032/q2_2.ml
+++ 2/2-141032/q2_2.ml
@@ -3,20 +3,20 @@ type bnat = B | O of bnat | I of bnat;;
 exception Negative_number;;
 
 let rec add n1 n2 =
-  match (n1, n2) with
-  | (B, m) | (m, B) -> m
-  | (O m1, O m2) -> O (add m1 m2)
-  | (O m1, I m2) | (I m1, O m2) -> I (add m1 m2)
-  | (I m1, I m2) -> O (add (add m1 (I B)) m2);;
+  match n1, n2 with
+  | B, m | m, B -> m
+  | O m1, O m2 -> O (add m1 m2)
+  | O m1, I m2 | I m1, O m2 -> I (add m1 m2)
+  | I m1, I m2 -> O (add (add m1 (I B)) m2);;
 
 let rec sub n1 n2 =
   let n = match (n1, n2) with
-    | (_, B) -> n1
-    | (B, _) -> raise Negative_number
-    | (O m1, O m2) -> O (sub m1 m2)
-    | (O m1, I m2) -> I (sub (sub m1 (I B)) m2)
-    | (I m1, O m2) -> I (sub m1 m2)
-    | (I m1, I m2) -> O (sub m1 m2)
+    | _, B -> n1
+    | B, _ -> raise Negative_number
+    | O m1, O m2 -> O (sub m1 m2)
+    | O m1, I m2 -> I (sub (sub m1 (I B)) m2)
+    | I m1, O m2 -> I (sub m1 m2)
+    | I m1, I m2 -> O (sub m1 m2)
   in
   match n with
   | O B -> B
