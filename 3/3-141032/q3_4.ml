e8d7fd0f3b0ad9e0ddd0ffe3de18ac7a7737b88e
yahiro.wataru <wtr816@gmail.com>
2014-05-04 11:46:19 +0900

    q3_4

---
+++ 3/3-141032/q3_4.ml
@@ -0,0 +1,76 @@
+#use "orderd_type.ml";;
+   
+module Dict (K : ORDERED_TYPE) :
+sig
+  type 'a t
+  val empty : 'a t
+  val add : K.t -> 'a -> 'a t -> 'a t
+  val remove_min : 'a t -> K.t option * 'a option * 'a t
+  val remove : K.t -> 'a t -> 'a t
+  val lookup : K.t -> 'a t -> 'a option
+  val size : 'a t -> int
+  val show : 'a t -> (K.t * 'a) list
+  val load : (K.t * 'a) list -> 'a t -> 'a t
+end = 
+  struct
+    type 'a t = Leaf | Node of K.t * 'a * 'a t * 'a t
+						    
+    let empty = Leaf
+		  
+    let rec add k v tree =
+      match tree with
+      | Leaf -> Node (k, v, Leaf, Leaf)
+      | Node (l, u, tl, tr) ->
+    	 match K.compare k l with
+    	 | EQ -> Node (l, v, tl, tr)
+    	 | LT -> Node (l, u, add k v tl, tr)
+    	 | GT -> Node (l, u, tl, add k v tr)
+
+    let rec remove_min tree =
+      match tree with
+      | Leaf -> (None, None, Leaf)
+      | Node (k, v, Leaf, tr) -> (Some k, Some v, tr)
+      | Node (k, v, tl, tr) ->
+    	 let (l, u, t) = remove_min tl
+    	 in
+    	 (l, u, Node (k, v, t, tr))
+
+    let rec remove k tree =
+      match tree with
+      | Leaf -> Leaf
+      | Node (l, u, tl, tr) ->
+    	 match K.compare k l with
+    	 | EQ ->
+    	    if tl = Leaf then tr
+    	    else
+    	      (match (remove_min tr) with
+    	       | (None, _, _) -> tl
+    	       | (Some m, Some w, t) -> Node (m, w, tl, t))
+    	 | LT -> Node (l, u, remove k tl, tr)
+    	 | GT -> Node (l, u, tl, remove k tr)
+
+    let rec lookup k tree =
+      match tree with
+      | Leaf -> None
+      | Node (l, u, tl, tr) ->
+    	 match K.compare k l with
+    	 | EQ -> Some u
+    	 | LT -> lookup k tl
+    	 | GT -> lookup k tr
+
+    let rec size tree =
+      match tree with
+      | Leaf -> 0
+      | Node (k, u, tl, tr) -> 1 + (size tl) + (size tr)
+
+    let show tree =
+      let rec show_aux tree ps =
+  	match tree with
+  	| Leaf -> ps
+  	| Node (k, v, tl, tr) -> show_aux tr ((k, v)::(show_aux tl ps))
+      in
+      List.rev (show_aux tree [])
+	       
+    let load ps tree =
+      List.fold_left (fun t p -> add (fst p) (snd p) t) tree ps
+  end
--- 3/3-141032/test_q3_4.ml
+++ 3/3-141032/test_q3_4.ml
@@ -1,1 +1,12 @@
 #use "q3_4.ml";;
+
+module StrDict = Dict(OrderedString);;
+
+let data = [("ten", 10); ("four", 4); ("nine", 9); ("six", 6); ("eleven", 11); ("two", 2); ("fourteen", 14); ("thirteen", 13); ("zero", 0); ("five", 5); ("eight", 8); ("seven", 7); ("one", 1); ("twelve", 12); ("three", 3)];;
+let s = StrDict.load data StrDict.empty;;
+StrDict.show s;;
+StrDict.lookup "one" s;;
+let s = StrDict.remove "ten" s;;
+let s = StrDict.remove "one" s;;
+StrDict.show s;;
+StrDict.lookup "one" s;;
