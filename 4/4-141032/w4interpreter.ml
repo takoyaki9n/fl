5dfd4736a46b2b2af669e7ebd1b915c4e9d73f47
yahiro.wataru <wtr816@gmail.com>
2014-05-14 00:15:50 +0900

    q2 to q4

--- 4/4-141032/w4interpreter.ml
+++ 4/4-141032/w4interpreter.ml
@@ -2,36 +2,48 @@ open Syntax
 
 exception Eval_error of string;;
   
-let rec eval e = 
-  match e with
+let empty_env = [];;
+
+let rec eval_expr env = function
   | EConst v -> v
+  | EVar (Name v) -> 
+     (try
+	 List.assoc v env
+       with
+       |Not_found -> raise (Eval_error "unbound variable"))
   | EAdd (e1, e2) -> 
-     (match (eval e1), (eval e2) with
+     (match (eval_expr env e1), (eval_expr env e2) with
       | VInt v1, VInt v2 -> VInt (v1 + v2)
       | _ -> raise (Eval_error "add: arguments must be (int, int)"))
   | ESub (e1, e2) -> 
-     (match (eval e1), (eval e2) with
+     (match (eval_expr env e1), (eval_expr env e2) with
       | VInt v1, VInt v2 -> VInt (v1 - v2)
       | _ -> raise (Eval_error "sub: arguments must be (int, int)"))
   | EMul (e1, e2) -> 
-     (match (eval e1), (eval e2) with
+     (match (eval_expr env e1), (eval_expr env e2) with
       | VInt v1, VInt v2 -> VInt (v1 * v2)
       | _ -> raise (Eval_error "mul: arguments must be (int, int)"))
   | EDiv (e1, e2) -> 
-     (match (eval e1), (eval e2) with
+     (match (eval_expr env e1), (eval_expr env e2) with
       | VInt v1, VInt 0 -> raise (Eval_error "div: division by zero")
       | VInt v1, VInt v2 -> VInt (v1 / v2)
       | _ -> raise (Eval_error "div: arguments must be (int, int)"))
   | EEq (e1, e2) -> 
-     (match (eval e1), (eval e2) with
+     (match (eval_expr env e1), (eval_expr env e2) with
       | VInt v1, VInt v2 -> VBool (v1 = v2)
       | VBool v1, VBool v2 -> VBool ((v1 && v2) || (not (v1 || v2)))
       | _ -> raise (Eval_error "equal: arguments must be (int, int) or (bool, bool)"))
-  | ELt (e1, e2) -> 
-     (match (eval e1), (eval e2) with
+  | ELT (e1, e2) -> 
+     (match (eval_expr env e1), (eval_expr env e2) with
       | VInt v1, VInt v2 -> VBool (v1 < v2)
       | _ -> raise (Eval_error "equal: arguments must be (int, int)"))
   | EIf (e1, e2, e3) -> 
-     (match (eval e1) with
-      | VBool v1  -> if v1 then (eval e2) else (eval e3)
-      | _ -> raise (Eval_error "if: arguments must be (bool, value, value)"));;
+     (match (eval_expr env e1) with
+      | VBool v1  -> if v1 then (eval_expr env e2) else (eval_expr env e3)
+      | _ -> raise (Eval_error "if: arguments must be (bool, value, value)"))
+  | ELet (Name k, e1, e2) ->
+     let v1 = eval_expr env e1 in
+     let env = (k, v1)::env in
+     eval_expr env e2
+  | _ -> raise (Eval_error "eval failed");;
+
