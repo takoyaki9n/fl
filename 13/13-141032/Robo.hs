c1ae3528f0ec2067bec4022f0e4740ecfec4ced4
yahiro.wataru <wtr816@gmail.com>
2014-07-18 23:51:20 +0900

    robo a_star

--- 13/13-141032/Robo.hs
+++ 13/13-141032/Robo.hs
@@ -1,8 +1,9 @@
 module Robo where
 import Data.Map hiding (size, map, filter, foldl)
+import Data.List hiding (insert)
 
 --{-
-f1 = Field (20, 10) (14 ,2) [(12,1),(8,2),(12,2),(12,3),(8,3),(8,4),(7,5),(8,5),(9,5),(10,5)]
+f1 = Field (20, 10) (14 ,2) [(12,0), (12,1),(8,2),(12,2),(12,3),(8,3),(8,4),(7,5),(8,5),(9,5),(10,5)]
 f2 = Field (20, 10) (14 ,2) [(13,1),(14,1),(15,1),(8,2),(9,2),(10,2),(11,2),(12,2),(13,2),(15,2),(8,3),(8,4),(8,5),(8,6)]
 f3 = Field (3, 3) (2 ,2) [(2, 0), (1, 1), (0, 2)]
 ---}
@@ -32,9 +33,25 @@ showField f ps =
                                    | otherwise -> '_') [0..sx - 1]
                  in l++"\n") [0..sy - 1]
 
-data Cell = Cell {dist :: Integer, pos :: Point, link :: Maybe Cell}
+data Cell = Cell {dist_to :: Integer, dist_from :: Integer, pos :: Point, link :: Maybe Cell}
 data Robot = Robot {runNext :: Field -> Point -> [Point], 
                     runHeuristic :: Field -> Point -> Integer}
+
+plus = Robot (\f (x, y) -> 
+               let ps = map (\(dx, dy) -> (x + dx, y + dy)) [(1,0),(0,1),(-1,0),(0,-1)]
+               in filter (movable f) ps)
+             (\f (x, y) -> 
+               let (gx, gy) = goal f in (abs (gx - x) + abs (gy - y)))
+king = Robot (\f (x, y) -> 
+               let ps = map (\(dx, dy) -> (x + dx, y + dy)) [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
+               in filter (movable f) ps)
+             (\f (x, y) -> 
+               let (gx, gy) = goal f in max (abs (gx - x)) (abs (gy - y)))
+kinght = Robot (\f (x, y) -> 
+               let ps = map (\(dx, dy) -> (x + dx, y + dy)) [(2,1),(1,2),(-1,2),(-2,1),(-2,-1),(-1,-2),(1,-2),(2,-1)]
+               in filter (movable f) ps)
+             (\f (x, y) -> 
+               let (gx, gy) = goal f in (div (abs (gx - x) + abs (gy - y) - 1) 3) + 1)
 
 backtrack :: Cell -> [Point]
 backtrack c = go c []
@@ -50,22 +67,41 @@ dijkstra f r s =
     []  -> []
     x:_ -> backtrack x
   where
-    start = Cell 0 s Nothing
+    start = Cell 0 0 s Nothing
     queue = start: go 1 (insert s start empty) queue
     go 0 _  _      = []
     go n mp (x:xs) = nexts ++ (go (n + length nexts - 1) mp' xs)
       where
         p = pos x
-        d = dist x
+        d = dist_to x
         ps = filter (\q -> 
                       case Data.Map.lookup q mp of
                         Nothing -> True
-                        Just y  -> d + 1 < dist y) (runNext r f p)
-        nexts = map (\q -> Cell (d + 1) q (Just x)) ps
+                        _       -> False) (runNext r f p)
+        nexts = map (\q -> Cell (d + 1) 0 q (Just x)) ps
         mp' = foldl (\m y -> insert (pos y) y m) mp nexts
 
-plus = Robot (\f (x, y) -> 
-                      let ps = map (\(dx, dy) -> (x + dx, y + dy)) [(1, 0),(0, 1),(-1, 0),(0, -1)]
-                      in filter (movable f) ps)
-             (\f (x, y) -> let (gx, gy) = goal f in (abs (gx - x) + abs (gy - y)))
-
+a_star :: Field -> Robot -> Point -> [Point]
+a_star f r s = go (insert s start empty) [start]
+  where
+    start = Cell 0 (runHeuristic r f s) s Nothing
+    go _  []     = []
+    go mp (x:xs) = 
+      if pos x == goal f then
+        backtrack x
+      else
+        go mp' (merge nexts xs)
+      where
+        p = pos x
+        d = dist_to x
+        ps = filter (\q -> 
+                      case Data.Map.lookup q mp of
+                        Nothing -> True
+                        _       -> False) (runNext r f p)
+        nexts = Data.List.sortBy (\a b -> if dist_from a < dist_from b then LT else GT) $ map (\q -> Cell (d + 1) (runHeuristic r f q) q (Just x)) ps
+        mp' = foldl (\m y -> insert (pos y) y m) mp nexts
+        merge [] xs = xs
+        merge xs [] = xs
+        merge (x:xs) (y:ys)
+          | dist_to x + dist_from x < dist_to y + dist_from y = x:(merge xs (y:ys))
+          | otherwise = y:(merge (x:xs) ys)
